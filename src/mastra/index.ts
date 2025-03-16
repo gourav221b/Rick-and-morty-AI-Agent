7d7ecc07993314c5dfb75ba14ba8c34d62f66ea7
Gourav Ghosal <slggourav@gmail.com>
2025-03-16 12:51:45 +0530

    added netlify deployer

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,12 +1,17 @@
 
 import { Mastra } from '@mastra/core';
 import { rickMorty } from './agents';
-
+import { NetlifyDeployer } from '@mastra/deployer-netlify';
 export const mastra = new Mastra(
     {
         agents: {
             rickMorty
         },
+        deployer: new NetlifyDeployer({
+            scope: `${process.env.NETLIFY_SCOPE}`,
+            projectName: `${process.env.NETLIFY_PROJECT_NAME}`,
+            token: `${process.env.NETLIFY_TOKEN}`
+        }),
 
     }
 )
